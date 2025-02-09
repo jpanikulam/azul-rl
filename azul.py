7fc4d411a6eca3c790c69ee727600a9fffe32c7f
jpanikulam <j@mainstreetautonomy.com>
2025-02-09 00:50:29 -0500

    Fix bugs

--- azul.py
+++ azul.py
@@ -17,7 +17,7 @@ class GameConfig(object):
         self.round = 1
         self.n_colors = 5
         self.n_factory_displays = 1
-        self.n_tiles_per_factory_display = 4
+        self.n_tiles_per_factory_display = 8 # was 4
         self.n_rows = self.n_colors
 
 def create_empty_board(cfg: GameConfig):
@@ -32,8 +32,9 @@ class BoardState(object):
         self.players = []
         for n in range(cfg.players):
             self.players.append({
-                'board': create_empty_board(cfg),
-                'pending': [(-1, 0) for _ in range(cfg.n_rows)],
+                "board": create_empty_board(cfg),
+                "pending": [(-1, 0) for _ in range(cfg.n_rows)],
+                "score": 0
             })
 
         # N factory displays * M colors
@@ -64,12 +65,12 @@ class BoardState(object):
         self.curr_step = 0
 
     def draw(self):
-        colors = ['r', 'g', 'b', 'y', 'c', 'm']
+        colors = ["r", "g", "b", "y", "c", "m"]
 
         for n, player in enumerate(self.players):
             plt.figure(f"Player {n}")
             ax = plt.gca()
-            for x, board_row in enumerate(player['board']):
+            for x, board_row in enumerate(player["board"]):
                 for y, count in enumerate(board_row):
                     color = colors[(y + x) % len(board_row)]
                     scale= (1.0 / len(board_row))
@@ -92,7 +93,7 @@ class BoardState(object):
                             0.9*scale,
                             linewidth=1,
                             edgecolor=color,
-                            facecolor='none')
+                            facecolor="none")
                         ax.add_patch(rect)
 
 
@@ -112,10 +113,10 @@ class BoardState(object):
             for source_color, count in enumerate(self.factory_displays[factory_display]):
                 if count != 0:
                     for destination_color in range(cfg.n_colors):
-                        (pending_color, count) = self.players[player_idx]['pending'][destination_color]
+                        (pending_color, count) = self.players[player_idx]["pending"][destination_color]
                         nothing_pending = pending_color == -1
                         pending_matches = pending_color == source_color
-                        board_spot_unoccupied = self.players[player_idx]['board'][pending_color][source_color] == 0
+                        board_spot_unoccupied = self.players[player_idx]["board"][pending_color][source_color] == 0
                         if (nothing_pending or pending_matches) and board_spot_unoccupied:
                             actions.append((factory_display, source_color, destination_color))
         return actions
@@ -127,42 +128,55 @@ class BoardState(object):
         player_idx = self.curr_step % len(self.players)
 
         # number of colors is the length of the first column
-        n_colors = len(self.players[player_idx]['board'][0])
+        n_colors = len(self.players[player_idx]["board"][0])
 
         if factory_display < 0 or factory_display >= len(self.factory_displays):
-            return (False,  'Invalid factory display')
+            return (False, "Invalid factory display")
         chosen_display = self.factory_displays[factory_display]
 
         n_added = chosen_display[color]
 
         if n_added == 0:
-            return (False,  'Tile not present in display')
+            return (False, "Tile not present in display")
 
-        chosen_display[color] = 0
-
-        (current_tile_id, current_count) = self.players[player_idx]['pending'][destination_row]
+        (current_tile_id, current_pending) = self.players[player_idx]["pending"][destination_row]
 
         if current_tile_id != -1 and current_tile_id != color:
-            return (False,  'Tile not present in location')
+            return (False, "Tile not present in location")
 
-        if current_count > destination_row + 1:
-            return (False,  'No room for in pending')
 
-        if self.players[player_idx]['board'][destination_row][(color + destination_row) % n_colors] != 0:
-            return (False,  'Tile already populated')
+        # Handle overflow
+        REJECT_OVERFLOW = True
+        if REJECT_OVERFLOW:
+            if current_pending + n_added > destination_row + 1:
+                return (False, "Would overflow pending tiles")
+        elif current_pending + n_added > destination_row + 1:
+            residual = n_added + current_pending - (destination_row + 1)
+            print(f"Added: {n_added}")
+            n_added = min(n_added + current_pending, destination_row + 1)
+            self.players[player_idx]["score"] -= residual
 
-        self.players[player_idx]['pending'][destination_row] = (color, current_count + n_added)
+        if self.players[player_idx]["board"][destination_row][(color + destination_row) % n_colors] != 0:
+            return (False, "Tile already populated")
+
+        chosen_display[color] = 0
+        self.players[player_idx]["pending"][destination_row] = (color, current_pending + n_added)
         self.curr_step += 1
         return (True, "Success")
 
     def step(self, cfg: GameConfig):
         for idx, player in enumerate(self.players):
-            for row_idx, row in enumerate(player['pending']):
-                (color, count) = row
-                if count == color + 1:
-                    print(f"Stepping {row_idx} {color}")
-                    self.players[idx]['board'][row_idx][(row_idx + color) % cfg.n_colors] += 1
-                    self.players[idx]['pending'][row_idx] = (-1, 0)
+            for row_idx, pending_row in enumerate(player["pending"]):
+                (color, count) = pending_row
+                print(f"Color: {color}, count: {count}, row: {row_idx}")
+                if count == row_idx + 1 and color != -1:
+                    col_idx = (row_idx + color) % cfg.n_colors
+                    player["board"][row_idx][col_idx] += 1
+                    player["score"] += 1
+                    col_populated = all([player["board"][row_idx][col_idx] for row_idx in range(cfg.n_colors)])
+                    if col_populated:
+                        player["score"] += 7
+                    player["pending"][row_idx] = (-1, 0)
 
     def is_factory_empty(self):
         for factory_display in self.factory_displays:
@@ -177,9 +191,8 @@ if __name__ == "__main__":
     game_state = BoardState(game_cfg)
     game_state.start_round(game_cfg)
     game_state.print()
-    print(game_state.grab_and_place_tile(factory_display=0, color=0, row=1))
+    print(game_state.grab_and_place_tile(factory_display=0, color=1, destination_row=2))
     game_state.print()
-    print(game_state.available_actions(cfg=game_cfg))
     game_state.step(cfg=game_cfg)
     game_state.print()
     game_state.draw()
